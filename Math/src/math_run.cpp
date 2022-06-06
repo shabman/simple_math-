cc4805e3870bfc9f42f5d6bc2ad261f31b54743d
shabman <59781727+shabman@users.noreply.github.com>
2022-06-06 17:04:38 +0100

    Update math_run.cpp

--- Math/src/math_run.cpp
+++ Math/src/math_run.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
 #include "../calc/simple_math.h"
+#include "../manipulation/matrix.h"
+#include "../indices/bounds.h"
+
+int Bounds::target = 3;
+std::array<double, Bounds::BOUND_SIZE> b = Bounds::getBounds();
 
 int main() {
     printf("%d\n", strict_add(2, 2));
@@ -10,5 +15,18 @@ int main() {
     printf("%d\n", strict_flr(4.3));
     printf("%d\n", strict_ceil(7.5));
     printf("%d\n", strict_abs(210310 + 1903391));
+    // Manipulation
+    printf("%s\n", "Manipulation");
+    Matrix m {};
+    m.a = 11;
+    m.b = 3;
+    m.c = 4;
+    m.d = 9;
+    printf("%d\n", m.get_determinant());
+    // Indices
+    printf("%s\n", "Indices");
+    printf("%f\n", b[0]);
+    printf("%f\n", b[1]);
+    printf("%f\n", b[2]);
     return 0;
 }
